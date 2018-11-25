c648c91d0fa2b2082f34467aec6f77c1499a6499
jackrex <jackrex1993@gmail.com>
2018-11-25 22:41:41 +0800

    add lr

--- L4/SentimentClassification.py
+++ L4/SentimentClassification.py
@@ -1,3 +1,50 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
+# pip3 install -U scikit-learn
+
+from sklearn import datasets
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.linear_model import LogisticRegression
+
+MOVIE_PATH = '/Users/jackrex/Desktop/AILesson/L4/aclImdb/train'
+MOVIE_TEST_PATH = '/Users/jackrex/Desktop/AILesson/L4/aclImdb/test'
+train_movie_data = datasets.load_files(MOVIE_PATH, 'Movie Comments', ['neg', 'pos'], True, True, None, 'strict', 42)
+test_movie_data = datasets.load_files(MOVIE_TEST_PATH, 'Movie Test Comments', ['neg', 'pos'], True, True, None,
+                                    'strict', 42)
+
+def classifier_report(classifier):
+    count_vec = CountVectorizer()
+    x_train = count_vec.fit_transform(train_movie_data.data)
+
+    x_test = count_vec.transform(test_movie_data.data)
+
+    classifier.fit(x_train, train_movie_data.target)
+
+    predicted = classifier.predict(x_test)
+    print(classification_report(test_movie_data.target, predicted, target_names=train_movie_data.target_names))
+
+def split_bayesian():
+    x_train, x_test, y_train, y_test = train_test_split(train_movie_data.data, train_movie_data.target, test_size=0.25, random_state=33)
+    count_vec = CountVectorizer()
+    x_count_train = count_vec.fit_transform(x_train)
+    x_count_test = count_vec.transform(x_test)
+
+    mnb_count = MultinomialNB()
+    mnb_count.fit(x_count_train, y_train)
+    y_count_predict = mnb_count.predict(x_count_test)
+    print(classification_report(y_test, y_count_predict, target_names=train_movie_data.target_names))
+
+
+if __name__ == '__main__':
+    bayesian = MultinomialNB()
+    logistic_regression = LogisticRegression(random_state=0, solver='lbfgs')
+
+    # classifier_report(bayesian)
+    # split_bayesian()
+    classifier_report(logistic_regression)
+
+
 
