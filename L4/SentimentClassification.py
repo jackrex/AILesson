54036de48e8fe701a900663c2ac125eef2bedae8
jackrex <jackrex1993@gmail.com>
2018-11-26 20:48:32 +0800

    fix bug

--- L4/SentimentClassification.py
+++ L4/SentimentClassification.py
@@ -40,7 +40,7 @@ def split_bayesian():
 
 if __name__ == '__main__':
     bayesian = MultinomialNB()
-    logistic_regression = LogisticRegression(random_state=0, solver='lbfgs')
+    logistic_regression = LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=1.0,fit_intercept=True, intercept_scaling=1, class_weight='balanced', random_state=None, solver='sag', max_iter=10000, multi_class='ovr', verbose=0, warm_start=False, n_jobs=10)
 
     # classifier_report(bayesian)
     # split_bayesian()
