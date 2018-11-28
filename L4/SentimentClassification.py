53c3696d7370c6c4f471b3f4b75db4fbb0d7c8b2
jackrex <jackrex1993@gmail.com>
2018-11-28 20:48:06 +0800

    add random cv

--- L4/SentimentClassification.py
+++ L4/SentimentClassification.py
@@ -1,7 +1,7 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 # pip3 install -U scikit-learn
-
+from scipy.stats import uniform
 from sklearn import datasets
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
@@ -9,9 +9,11 @@ from sklearn.metrics import classification_report
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegressionCV
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
 
@@ -35,8 +37,10 @@ stop_words = pandas.read_csv("stopwords.txt", delimiter="\n", header=None)
 def classifier_report(classifier):
     t = time.time()
     print(type(classifier))
-    count_vec = CountVectorizer()
+    count_vec = TfidfVectorizer(max_features=None, strip_accents='unicode', analyzer='word',token_pattern=r'\w{1,}', ngram_range=(1, 2))
     x_train = count_vec.fit_transform(train_movie_data.data)
+    # print('weight is ' + str(x_train.toarray()))
+    # print(count_vec.get_feature_names())
     x_test = count_vec.transform(test_movie_data.data)
 
     classifier.fit(x_train, train_movie_data.target)
@@ -63,7 +67,7 @@ def split_bayesian():
 
 def optimize_logistic_regression():
     # classifier = LogisticRegression()
-    classifier = LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=1.0,fit_intercept=True, intercept_scaling=1, class_weight='balanced', random_state=None, solver='sag', max_iter=2000, multi_class='ovr', verbose=0, warm_start=False, n_jobs=-1)
+    classifier = LogisticRegression(penalty='l2', dual=False, tol=1e-6, C=1.0,fit_intercept=True, intercept_scaling=1, class_weight=None, random_state=None, solver='liblinear', max_iter=1000, multi_class='ovr', verbose=0, warm_start=False, n_jobs=-1)
     count_vec = CountVectorizer()
     x_train = count_vec.fit_transform(train_movie_data.data)
     print('x_train shape is ' + str(x_train.shape))
@@ -77,13 +81,13 @@ def optimize_logistic_regression():
 
     x_test = count_vec.transform(test_movie_data.data)
     tfidf_test = transformer.transform(x_test)
-    print('x_test shape is ' + str(x_test.shape))
+    print('x_test shape is ' + str(tfidf_test.shape))
 
-    # tuned_parameters = {'C': scipy.stats.expon(scale=100),
+    # tuned_parameters = {'C': uniform(loc=0, scale=4),
     #                     'multi_class': ['ovr', 'multinomial']}
     #
     # classifier = RandomizedSearchCV(LogisticRegression(penalty='l2', solver='lbfgs', tol=1e-6),
-    #                          tuned_parameters, cv=10, scoring='accuracy', n_iter=1000)
+    #                          tuned_parameters, cv=10, scoring='accuracy', n_iter=30)
 
 
 
@@ -109,19 +113,21 @@ def optimize_logistic_regression():
 
 if __name__ == '__main__':
 
-    # bayesian = MultinomialNB()
-    # logistic_regression = LogisticRegression()
-    # random_forest = RandomForestClassifier()
-    # decision_tree = DecisionTreeClassifier()
-    #
-    # split_bayesian()
-    #
-    # classifier_report(bayesian)
-    # classifier_report(logistic_regression)
-    # classifier_report(random_forest)
-    # classifier_report(decision_tree)
+    bayesian = MultinomialNB()
+    logistic_regression = LogisticRegression()
+    random_forest = RandomForestClassifier()
+    decision_tree = DecisionTreeClassifier()
+    logistic_cv = LogisticRegressionCV(cv=5, max_iter=100, n_jobs=-1)
 
-    optimize_logistic_regression()
+    # split_bayesian()
+
+    classifier_report(bayesian)
+    classifier_report(logistic_regression)
+    classifier_report(logistic_cv)
+    classifier_report(random_forest)
+    classifier_report(decision_tree)
+
+    # optimize_logistic_regression()
 
 
 
