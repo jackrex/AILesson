167b7c7942f7cd2171df2442ad1a60eeb356a3d1
jackrex <jackrex1993@gmail.com>
2019-01-23 22:39:57 +0800

    update readme

--- L9/LSTM.py
+++ L9/LSTM.py
@@ -2,4 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import keras
+import numpy
+import matplotlib.pyplot as plt
+from pandas import read_csv
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
 
+dataframe = read_csv('international-airline-passengers.csv', usecols=[1], engine='python', skipfooter=3)
+dataset = dataframe.values
+dataset = dataset.astype('float32')
+plt.plot(dataset)
+plt.show()
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # AILesson
 from know a little to give up
+
+# Structure
+- /Course contains the AI Lesson PDF such as DT, Bayes, LM etc.
+- /Release contains the homework of AI Lesson
+- L2 The Naive bayesian classification
+- L3 Decision Tree
+- L4 LogisticRegression
+- L5 SVM
+- L6 GM & EM Algorithm
+- L7 Topic Modeling
+- L8 CNN
+- L9 LSTM
